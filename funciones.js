87ef24dc49c7eea8ab987d88e752d3fb29ce163f
Christian Salazar <rockerson@rockeme.com>
2022-06-11 09:33:19 -0500

    Primer commit

---
+++ funciones.js
@@ -0,0 +1,79 @@
+var grupoComidasRapidas = ["🌭","🍔","🍕","🍟","🌮","🌯","🫓","🥪","🥙"];
+var grupoFrutas = ["🍎","🍐","🍓","🍇","🍉","🍌","🍊","🍒","🥥"];
+var grupoCompleto = grupoComidasRapidas.concat(grupoFrutas);
+var totalTarjetas = grupoCompleto.concat(grupoCompleto);
+
+function Barajar(){
+    var resultado;
+    
+    resultado = totalTarjetas.sort(function() {
+        return 0.5 - Math.random();
+    });
+
+    return resultado;
+}
+
+
+function Repartir(){
+    var tarjetasBarajadas = Barajar();
+    var mesa = document.querySelector("#mesa");
+    mesa.innerHTML = "";
+
+    tarjetasBarajadas.forEach(function(elemento){
+        var tarjeta = document.createElement("div");
+
+        tarjeta.innerHTML = 
+            '<div class="tarjeta" data-valor=' + elemento + ' >' + 
+                '<div class="tarjeta__contenido">' + 
+                    elemento + 
+                '</div>' +    
+            '</div>';
+
+        mesa.appendChild(tarjeta);
+    })
+}
+
+function Descubrir(){
+    var totalDescubiertas = document.querySelectorAll(".descubierta:not(.acertada)");
+
+    if(totalDescubiertas.length > 1){
+        return;
+    }
+
+    this.classList.add("descubierta");
+
+    var descubiertas = document.querySelectorAll(".descubierta:not(.acertada)");
+
+    if(descubiertas.length < 2){
+        return;
+    }
+
+    Comparar(descubiertas);
+}
+
+function Comparar(tarjetasAComparar){
+        if(tarjetasAComparar[0].dataset.valor === tarjetasAComparar[1].dataset.valor){
+            Acierto(tarjetasAComparar);
+        }else{
+            Error(tarjetasAComparar);
+        }
+
+}
+
+function Acierto(lasTarjetas){
+    lasTarjetas.forEach(function(elemento){
+        elemento.classList.add("acertada");
+    });
+}
+
+function Error(lasTarjetas){
+    lasTarjetas.forEach(function(elemento){
+        elemento.classList.remove("descubierta");
+    });
+}
+
+Repartir();
+
+document.querySelectorAll(".tarjeta").forEach(function(elemento){
+    elemento.addEventListener("click", Descubrir);
+});
