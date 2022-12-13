5a842542117aa73b8a811140cbd02a233b0815a7
Rockerson <rockerson@rockeme.com>
2022-12-13 18:38:41 -0500

    cambio en titulo

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ if(isset( $_POST['submit-score'])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>🧩 Memory Game 🧩</title>
+    <title>🔥McMemory⚡</title>
     <link rel="stylesheet" href="css/reseteo.css">
     <link rel="stylesheet" href="css/estilos.css">
 </head>
