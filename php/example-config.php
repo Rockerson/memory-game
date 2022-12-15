f7eae9c5c4288a9cee72e276260e699b39d5e868
Rockerson <rockerson@rockeme.com>
2022-12-15 17:59:56 -0500

    acutalizado config example y gitignore

---
+++ php/example-config.php
@@ -0,0 +1,12 @@
+<?php
+//Reporte de errores
+error_reporting(E_ALL);
+ini_set('display_error', 1);
+//Ruta del sitio
+define( 'SITE_URL', 'http://example.url.ext/memory-game');
+//Info Base de datos
+define( 'BD_HOST', 'DB_host');
+define( 'DB_USER', 'DB_user_name');
+define( 'DB_PASSWORD', 'DB_password');
+define( 'DB_DATABASE', 'DB_name');
+define( 'DB_PORT', 'DB_port');
